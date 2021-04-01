19755a1366433b4b38577aaaec45e6c8a3f05cb9
@abramnguyen <hongthanhnguyen85@gmail.com>
2021-04-01 17:27:15 +0700

    section 8 lec-58

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middlewares/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 //  @desc       Register user
@@ -51,6 +52,72 @@ exports.login = asyncHandler(async (req, res, next) => {
 
 });
 
+
+
+//  @desc       Get current logged in user
+//  @route      POST /api/v1/auth/me
+//  @access     Private
+exports.getMe = asyncHandler( async (req, res, next) => {
+
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        data: req.user
+    })
+
+});
+
+
+//  @desc       Forgot password
+//  @route      POST /api/v1/auth/forgotpassword
+//  @access     Public
+exports.forgotPassword = asyncHandler( async (req, res, next) => {
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if(!user) {
+        return next(new ErrorResponse('There is no user with that email', 404));
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    // Create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+    const message = `Please make a PUT request to: \n\n ${resetUrl} to reset password`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset token',
+            message
+        });
+
+        res.status(200).json({success: true, data: 'Email sent'});
+    } catch (error) {
+
+        console.log((error));
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false});
+
+        return next(new ErrorResponse('Email could not be sent', 500));
+        
+    }
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+
+});
+
+
 // Get token from model, create cookie and send response
 const sendTokentResponse = (user, statusCode, res) => {
     const token = user.getSignedJwtToken();
@@ -73,17 +140,3 @@ const sendTokentResponse = (user, statusCode, res) => {
         });
 
 }
-
-//  @desc       Get current logged in user
-//  @route      POST /api/v1/auth/me
-//  @access     Private
-exports.getMe = asyncHandler( async (req, res, next) => {
-
-    const user = await User.findById(req.user.id);
-
-    res.status(200).json({
-        success: true,
-        data: req.user
-    })
-
-});
--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -29,7 +29,21 @@ exports.getBootcamp = asyncHandler( async (req, res, next) => {
 //  @desc       Create new bootcamp
 //  @route      POST /api/v1/bootcamps
 //  @access     Private
-exports.createBootcamp = asyncHandler( async (req, res, next) => {    
+exports.createBootcamp = asyncHandler( async (req, res, next) => {
+    // Add user to request body
+    req.body.user = req.user.id;
+
+    // Check for published bootcamp
+    const publishedBootcamp = await Bootcamp.findOne({user: req.user.id});
+
+    // If the user is not an admin. They can only add one bootcamp
+    if(publishedBootcamp && req.user.role !== 'admin') {
+        return next(
+            new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`, 400)
+        );
+    }
+    
+
     const bootcamp = await Bootcamp.create(req.body);
 
     res.status(200).json({
@@ -42,14 +56,22 @@ exports.createBootcamp = asyncHandler( async (req, res, next) => {
 //  @route      PUT /api/v1/bootcamps/:id
 //  @access     Private
 exports.updateBootcamp = asyncHandler( async (req, res, next) => {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
+        let bootcamp = await Bootcamp.findById(req.params.id);
 
         if(!bootcamp) {
             return next(new ErrorResponse(`Bootcamp NOT found with id of ${req.params.id}`, 404));         
         }
+
+        // Make sure user is bootcamp owner  
+        if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+            return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401));
+
+        } 
+
+        bootcamp =await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
 
         res.status(200).json({ success: true, data: bootcamp});
 });
@@ -58,11 +80,17 @@ exports.updateBootcamp = asyncHandler( async (req, res, next) => {
 //  @route      DELETE /api/v1/bootcamps/:id
 //  @access     Private
 exports.deleteBootcamp = asyncHandler( async (req, res, next) => {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+        const bootcamp = await Bootcamp.findById(req.params.id);
 
         if(!bootcamp) {
             return next(new ErrorResponse(`Bootcamp NOT found with id of ${req.params.id}`, 404));         
         }
+
+         // Make sure user is bootcamp owner  
+         if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+            return next(new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`, 401));
+
+        } 
 
         bootcamp.remove(); // To trigger the middleware to cascading delete courses
 
@@ -108,6 +136,12 @@ exports.bootcampPhotoUpload = asyncHandler( async (req, res, next) => {
     if(!bootcamp) {
         return next(new ErrorResponse(`Bootcamp NOT found with id of ${req.params.id}`, 404));         
     }
+
+     // Make sure user is bootcamp owner  
+     if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401));
+
+    } 
 
     if (!req.files) {
         return next( new ErrorResponse(`Please upload a file`, 400));
